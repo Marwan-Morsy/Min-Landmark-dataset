24e2c3cf7e7bf7f700df13ebb635e35934a391f8
Marwan-Morsy <maromorsy1995@gmail.com>
2018-08-13 01:44:49 +0200

    Add files via upload

---
+++ dataset/landmark3d_v1.0/casa_loma/script.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 01:18:14 2018
+
+@author: marwan
+"""
+
+with open("list_db.txt") as f:
+    with open("train.txt", "w") as f1:
+        i = 0
+        for line in f:
+            i=i+1
+            if(i>600):
+                break
+            else:
+                f1.write(line)                     
+                
+                
+            
+            
+with open("list_test.txt") as e:
+    with open("test.txt", "w") as e1:
+        j = 0
+        for line in e:
+            j=j+1
+            if(j>250):
+                break
+            else:
+                e1.write(line) 
